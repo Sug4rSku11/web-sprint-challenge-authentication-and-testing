5faac37ef93c8367f51570b1bed740b3b6f5706b
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-17 14:42:20 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -18,18 +18,13 @@ const checkUsernameExists = async (req, res, next){
 }
 
 async function checkUsernameFree(req, res, next) {
-    try{
-      const users = await User.findBy({ username: req.body.username})
-      if(!users.length) {
-        next()
-      } else {
-        next({status: 422, message: 'Username taken'})
-      }
-  
-     } catch (err){
-       next(err)
-     }
     
+      const users = await User.findBy({ username: req.body.username})
+      if(users.length) {
+        next({status: 422, message: 'Username taken'})
+      } else {
+        next()
+      }
   }
 
   module.exports = { checkUsernameExists,
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -69,7 +69,8 @@ router.post('/login', checkUsernameExists, (req, res, next) => {
  .then(([user]) => { 
   if(user && bcrypt.compareSync(password, user.password)){
    const token = tokenBuilder(user)
-    res.status(200).json({ message: `welcome ${req.user.username}`, token})
+    res.status(200).json({ message: `welcome ${user.username}`
+                          , token})
    } else {
     res.status(401).json({ message: 'invalid credentials'})
   }
